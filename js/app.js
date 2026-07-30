833dd341036533115ddf0d8de41f85a4820ae3e3
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-29 19:45:12 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
+import { crearAcordes } from "./acordes.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    crearAcordes();
+
+});
+
+// ==========================
+// REGISTRAR SERVICE WORKER
+// ==========================
+
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker
+
+            .register("./service-worker.js")
+
+            .then(() => {
+
+                console.log("✅ PWA lista");
+
+            })
+
+            .catch(error => {
+
+                console.error(error);
+
+            });
+
+    });
+
+}
