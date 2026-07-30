2288adce99d98bf977287bf76e28013c40a9a2cc
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-29 19:47:44 -0600

    Update acordes.js

--- js/acordes.js
+++ js/acordes.js
@@ -1,1 +1,63 @@
+{
+    "version": "0.1.0",
 
+    "instrumentos": [
+
+        "guitarra",
+        "ukulele"
+
+    ],
+
+    "notas": [
+
+        {
+            "nombre": "Do",
+            "codigo": "C"
+        },
+        {
+            "nombre": "Do#",
+            "codigo": "C#"
+        },
+        {
+            "nombre": "Re",
+            "codigo": "D"
+        },
+        {
+            "nombre": "Re#",
+            "codigo": "D#"
+        },
+        {
+            "nombre": "Mi",
+            "codigo": "E"
+        },
+        {
+            "nombre": "Fa",
+            "codigo": "F"
+        },
+        {
+            "nombre": "Fa#",
+            "codigo": "F#"
+        },
+        {
+            "nombre": "Sol",
+            "codigo": "G"
+        },
+        {
+            "nombre": "Sol#",
+            "codigo": "G#"
+        },
+        {
+            "nombre": "La",
+            "codigo": "A"
+        },
+        {
+            "nombre": "La#",
+            "codigo": "A#"
+        },
+        {
+            "nombre": "Si",
+            "codigo": "B"
+        }
+
+    ]
+}
