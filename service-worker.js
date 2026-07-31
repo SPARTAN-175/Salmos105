8feb712dd584f72649f2761d2ae36ae538e57978
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-31 09:02:07 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Service Worker
 // ================================
 
-const CACHE_NAME = "salmos105-v2";
+const CACHE_NAME = "salmos105-v2.1";
 
 // Archivos que siempre estarán disponibles offline
 const ARCHIVOS = [
