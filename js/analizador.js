f9e471541cd683309dca7d062640e18f7865ac34
SPARTAN-175 <avengers_0077@hotmail.com>
2026-08-02 13:44:11 -0600

    Create analizador.js

---
+++ js/analizador.js
@@ -0,0 +1,28 @@
+// =====================================
+// SALMOS 115
+// Analizador musical
+// =====================================
+
+import { obtenerExtractor } from "./essentia.js";
+
+/*=====================================
+    ANALIZAR AUDIO
+=====================================*/
+
+export async function analizarAudio(audio, sampleRate){
+
+    console.log("🎼 Iniciando análisis...");
+
+    const extractor = obtenerExtractor();
+
+    console.log("✅ Extractor obtenido");
+
+    console.log(extractor);
+
+    console.log("SampleRate:", sampleRate);
+
+    console.log("Muestras:", audio.length);
+
+    return true;
+
+}
