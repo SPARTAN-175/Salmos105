5b664a6bd95b0a38f7c7349b5c033dabfa6f792c
SPARTAN-175 <avengers_0077@hotmail.com>
2026-08-01 23:14:26 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Service Worker
 // ================================
 
-const CACHE_NAME = "salmos115-v3.3";
+const CACHE_NAME = "salmos115-v3.4";
 
 // Archivos disponibles sin internet
 const ARCHIVOS = [
