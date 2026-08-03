42f3861f69f3cecf96809b8456a271e8d2c45d9b
SPARTAN-175 <avengers_0077@hotmail.com>
2026-08-03 11:09:59 -0600

    Update pitch.js

--- pitch/pitch.js
+++ pitch/pitch.js
@@ -1,1 +1,39 @@
+// =====================================
+// SALMOS 115
+// Detector de nota
+// =====================================
 
+import { frecuenciaANota } from "./frecuencia.js";
+
+/*=====================================
+    PRUEBA
+=====================================*/
+
+export function probarFrecuencias(){
+
+    const pruebas = [
+
+        440,
+        261.63,
+        329.63,
+        392.00
+
+    ];
+
+    console.log("🎵 Prueba de conversión Hz → Nota");
+
+    for(const frecuencia of pruebas){
+
+        console.table(
+
+            frecuenciaANota(
+
+                frecuencia
+
+            )
+
+        );
+
+    }
+
+}
