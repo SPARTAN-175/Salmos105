2ed843415e1527e18858102ae654030bf9371af2
SPARTAN-175 <avengers_0077@hotmail.com>
2026-08-03 11:45:46 -0600

    Update pitch.js

--- pitch/pitch.js
+++ pitch/pitch.js
@@ -4,6 +4,35 @@
 // =====================================
 
 import { frecuenciaANota } from "./frecuencia.js";
+import { obtenerWASM } from "../js/essentia.js";
+
+/*=====================================
+    INSPECCIONAR WASM
+=====================================*/
+
+export function inspeccionarWASM(){
+
+    const wasm = obtenerWASM();
+
+    console.log("🎼 WASM:");
+
+    console.log(wasm);
+
+    console.log("🎼 Métodos relacionados con Pitch:");
+
+    console.log(
+
+        Object.keys(wasm)
+
+            .filter(nombre =>
+
+                nombre.toLowerCase().includes("pitch")
+
+            )
+
+    );
+
+}
 
 /*=====================================
     DETECTAR NOTA
