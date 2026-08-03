47a28c310248f833877cc6683b61031980a3d1d6
SPARTAN-175 <avengers_0077@hotmail.com>
2026-08-03 11:08:46 -0600

    Update frecuencia.js

--- pitch/frecuencia.js
+++ pitch/frecuencia.js
@@ -1,1 +1,80 @@
+// =====================================
+// SALMOS 115
+// Conversión Hz -> Nota
+// =====================================
 
+import { NOTAS } from "./notas.js";
+
+/*=====================================
+    FRECUENCIA A NOTA
+=====================================*/
+
+export function frecuenciaANota(frecuencia){
+
+    if(
+
+        frecuencia <= 0 ||
+
+        !isFinite(frecuencia)
+
+    ){
+
+        return null;
+
+    }
+
+    // --------------------------
+    // MIDI
+    // --------------------------
+
+    const midi = Math.round(
+
+        69 +
+
+        12 *
+
+        Math.log2(
+
+            frecuencia / 440
+
+        )
+
+    );
+
+    // --------------------------
+    // NOTA
+    // --------------------------
+
+    const indice =
+
+        ((midi % 12) + 12) % 12;
+
+    const nota =
+
+        NOTAS[indice];
+
+    // --------------------------
+    // OCTAVA
+    // --------------------------
+
+    const octava =
+
+        Math.floor(
+
+            midi / 12
+
+        ) - 1;
+
+    return {
+
+        frecuencia,
+
+        midi,
+
+        nota,
+
+        octava
+
+    };
+
+}
